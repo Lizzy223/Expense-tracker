15f049721ce6a01ef237a3a9e42efccd00be0e35
Lizzie <koladeelizabeth01@gmail.com>
2022-05-26 01:44:52 -0700

    first

--- src/components/Table2.js
+++ src/components/Table2.js
@@ -123,16 +123,6 @@ const Table2 = () =>{
                             )
                         })
                     }
-                    <Tr>
-                        <Td>feet</Td>
-                        <Td>centimetres (cm)</Td>
-                        <Td isNumeric>30.48</Td>
-                    </Tr>
-                    <Tr>
-                        <Td>yards</Td>
-                        <Td>metres (m)</Td>
-                        <Td isNumeric>0.91444</Td>
-                    </Tr>
                     </Tbody>
                     
                 </Table>
